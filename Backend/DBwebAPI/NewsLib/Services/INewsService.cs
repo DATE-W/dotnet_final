eca6976748f473f3139f47cff7d75f8cd94300da
DATE-W <100034299+DATE-W@users.noreply.github.com>
2024-06-28 06:15:44 +0800

    news lib2

---
+++ Backend/DBwebAPI/NewsLib/Services/INewsService.cs
@@ -0,0 +1,17 @@
+﻿using NewsLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsLib.Services
+{
+    public interface INewsService
+    {
+        public Task<CustomResponse> Init();
+        public Task<CustomResponse> GetNewsNum();
+        public Task<CustomResponse> GetNews(GetNewsRequest json);
+        public Task<CustomResponse> SearchNews(SearchNewsRequest json);
+    }
+}
